5811e576f6b0ad739f76246cccda5981bdbab701
Aarón Sánchez <aaronsanchezmenendez@gmail.com>
2023-11-20 10:49:59 +0000

    Bug fixing and -p _ for scan the whole range of ports

--- pmap.py
+++ pmap.py
@@ -3,56 +3,43 @@
 # @author: rxfatalslash
 
 import os
-import datetime
+import re
+from datetime import datetime
 import argparse as arg
 import socket as soc
 
-def main():
-    args = parse_arguments()
-    date = datetime.datetime.now().strftime("%Y-%m-%d %X")
-
-    print(f"Escaner de puertos Pmap 1.0 {date}")
-    
-    if not args.noping:
-        if ping(args.target, args.verbose):
-            print(f"Informe del escaneo de {args.target}\n")
-            print("HOST\t\tESTADO")
-            print(f"{args.target}\t\033[1;32mEncendido\033[0;m\n")
-
-            if ping(args.target, args.verbose) and args.ports:
-                print("PUERTO\t\tESTADO")
-                scan_ports(args.target, args.ports, args.open)
-        else:
-            print(f"Informe del escaneo de {args.target}\n")
-            print("HOST\t\tESTADO")
-            print(f"{args.target}\t\033[1;31mDesconectado\033[0;m\n")
-    else:
-        print(f"Informe del escaneo de {args.target}\n")
-        print("HOST\t\tESTADO")
-        print(f"{args.target}\t\033[1;32mEncendido\033[0;m\n")
-        print("PUERTO\t\tESTADO")
-        scan_ports(args.target, args.ports, args.open)
-
 def parse_arguments():
-    p = arg.ArgumentParser(description="Escaner de puertos escrito en Python, libre de dependencias externas")
-    p.add_argument("-t", "--target", type=str, required=True, help="Dirección IP del objetivo (obligatorio)")
-    p.add_argument("-p", "--ports", type=ports, default=[80], help="Puerto/s a escanear, puedes elegir uno o varios puertos separados por comas, o un rango de puertos separados por un guión, ej: (80,90) (100-1024). Por defecto se escaneará el puerto 80 si no se indica ningún puerto")
-    p.add_argument("-v", "--verbose", action="store_true", help="Activar verbosidad en la ejecucíon del script")
-    p.add_argument("--open", action="store_true", help="Mostrar solo los puertos abiertos")
-    p.add_argument("-Pn", "--noping", action="store_true", help="Esta opción omite la comprobación del estado del host mediante el ping, se da por hecho que el host está encendido. Útil para casos en los que el host tiene desactivados los ping pero se sabe que está encendido")
+    p = arg.ArgumentParser(description="Port scanner, free of external dependencies")
+    p.add_argument("-t", "--target", dest="ip", type=host, required=True, help="IP address to scan (required)")
+    p.add_argument("-p", "--ports", type=ports, default=[80], help="Port/s to scan, you are able to use multiple ports (,), ranges (-) and all ports (_)")
+    p.add_argument("-v", "--verbose", action="store_true", help="Activate verbosity")
+    p.add_argument("--open", action="store_true", help="Show only open ports")
+    p.add_argument("-Pn", "--noping", action="store_true", help="Skip the ping comprobation")
 
     return p.parse_args()
 
-def ping(target, verbose):
+def ping(ip, verbose):
     if verbose:
-        r = os.system(f"ping -c 1 {target}")
+        r = os.system(f"ping -c 1 {ip}")
     else:
-        r = os.system(f"ping -c 1 {target} 1>/dev/null")
+        r = os.system(f"ping -c 1 {ip} 1>/dev/null")
 
     if r == 0:
         return True
     else:
         return False
+
+
+def host(ip):
+    try:
+        if re.search("^([0-9]{1,3}\.){3}[0-9]{1,3}(\/[1-9][0-9]{1,2})?$", ip):
+            return ip
+        else:
+            print("Introduce a valid IP address")
+
+    except ValueError:
+        raise arg.ArgumentTypeError(f"Error analyzing hosts")
+
 
 def ports(ports):
     try:
@@ -61,29 +48,54 @@ def ports(ports):
         elif "-" in ports:
             start, end = map(int, ports.split("-"))
             return list(range(start, end + 1))
+        elif "_" in ports:
+            return list(range(1, 65536))
         else:
             return [int(ports)]
     
     except ValueError:
-        raise arg.ArgumentTypeError(f"Error al analizar los puertos")
+        raise arg.ArgumentTypeError(f"Error analyzing ports")
     
-def scan_ports(target, ports, open):
+def scan_ports(ip, ports, open):
     for port in ports:
         s = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
         s.settimeout(1)
 
         try:
-            s.connect((target, port))
+            s.connect((ip, port))
             if open:
-                print(f"{port}\t\t\033[1;32mAbierto\033[0;m")
+                print(f"{port}\t\t\033[1;32mOpen\033[0;m")
             else:
-                print(f"{port}\t\t\033[1;32mAbierto\033[0;m")
+                print(f"{port}\t\t\033[1;32mOpen\033[0;m")
 
             s.close()
         except soc.error:
             if not open:
-                print(f"{port}\t\t\033[1;31mCerrado\033[0;m")
-
+                print(f"{port}\t\t\033[1;31mClosed\033[0;m")
 
 if __name__ == "__main__":
-    main()
+    args = parse_arguments()
+    date = datetime.now().strftime("%Y-%m-%d %X")
+
+    print(f"Port scanner Pmap 1.0 {date}")
+    
+    if not args.noping:
+        if ping(args.ip, args.verbose):
+            print(f"Scan repot from {args.ip}\n")
+            print("HOST\t\tSTATE")
+            print(f"{args.ip}\t\033[1;32mUp\033[0;m\n")
+
+            if ping(args.ip, args.verbose) and args.ports:
+                print("PORT\t\tSTATE")
+                scan_ports(args.ip, args.ports, args.open)
+        else:
+            print(f"Scan report from {args.ip}\n")
+            print("HOST\t\tSTATE")
+            print(f"{args.ip}\t\033[1;31mDown\033[0;m\n")
+
+    else:
+        print(f"Scan report from {args.ip}\n")
+        print("HOST\t\tSTATE")
+        print(f"{args.ip}\t\033[1;32mUp\033[0;m\n")
+        print("PUERTO\t\tSTATE")
+        scan_ports(args.ip, args.ports, args.open)
